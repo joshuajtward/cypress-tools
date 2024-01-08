4310d7478e1f9ca582450f0b20a59e348c55d604
joshuajtward <joshuajtward+work@gmail.com>
2024-01-08 18:08:01 +0000

    more

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,13 @@
-export default {
-  defaultCommandTimeout: 1000,
-  screenshotOnRunFailure: false,
-  video: false,
-};
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    defaultCommandTimeout: 1000,
+    screenshotOnRunFailure: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: "cypress/integration/*.cy.js",
+    video: false,
+  },
+});
